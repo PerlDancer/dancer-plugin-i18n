1f9dbcaa36f283da7e54599f7d88229c5d753428
franck cuny <franck@lumberjaph.net>
2010-04-25 15:18:46 +0200

    initial commit

---
+++ lib/Dancer/Plugin/I18n.pm
@@ -0,0 +1,33 @@
+package Dancer::Plugin::I18n;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Dancer::Plugin::I18n -
+
+=head1 SYNOPSIS
+
+  use Dancer::Plugin::I18n;
+
+=head1 DESCRIPTION
+
+Dancer::Plugin::I18n is
+
+=head1 AUTHOR
+
+franck cuny E<lt>franck@lumberjaph.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
